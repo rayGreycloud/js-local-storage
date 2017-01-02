8effbf04d12589c24bb4fe77872761015482880b
rayGreycloud <rayGreycloud@gmail.com>
2017-01-02 12:22:58 -0800

    Add method to copy object and hack to clone object

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ const person = {
 }
 
 // Creates reference not copy just like array
-const sithLord = person;
-sithLord.allegiance = "Dark Side";
+// const sithLord = person;
+// sithLord.allegiance = "Dark Side";
+// console.log(sithLord);
+// console.log(person); // now has allegiance property too
+
+// Making a copy of Object (but only 1 lvl deep)
+const sithLord = Object.assign({}, person, {allegiance: 'Dark Side', social: {}});
 console.log(sithLord);
-console.log(person);
+console.log(person); // remains unchanged
+
+sithLord.social.twitter = '@darthvader';
+// Hack to clone Object
+const anakin = JSON.parse(JSON.stringify(sithLord));
+console.log(anakin);
 
 // const addItems = document.querySelector('.add-items');
 // const itemsList = document.querySelector('.plates');
