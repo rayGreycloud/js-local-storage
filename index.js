97add00b339efb2c832e1b110cf67e9e460dcc9b
rayGreycloud <rayGreycloud@gmail.com>
2017-01-02 12:10:35 -0800

    Add example of object reference vs copy problem

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
-// Array copying examples
-const players = ["Able", "Baker", "Charlie", "Delta"];
+// Object copying - same reference vs. copy issues
+const person = {
+  name: 'Darth Vader',
+  favoriteColor: 'Black'
+}
 
-// only creates reference, modifying it alters original
-const team1 = players;
-
-// Four ways to copy array and not just reference
-// #1 - Use slice()
-const team2 = players.slice();
-
-// #2 - Create new array and concat
-const team3 = [].concat(players);
-
-// #3 - Use spread operator
-const team4 = [...players];
-
-// #4 - Use Array.from
-const team5 = Array.from(players);
-
-console.log(team1);
-console.log(team2);
-console.log(team3);
-console.log(team4);
-console.log(team5);
+// Creates reference not copy just like array
+const sithLord = person;
+sithLord.allegiance = "Dark Side";
+console.log(sithLord);
+console.log(person);
 
 // const addItems = document.querySelector('.add-items');
 // const itemsList = document.querySelector('.plates');
